a95cb006a2b9a3354767259e7ca786563c2afcd0
Bruce Wayne <brucethebatmandev@gmail.com>
2024-08-02 09:33:55 +0100

    Gallery wip

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -2,19 +2,43 @@ import React from 'react'
 import { CarouselSize } from './Carousel'
 
 const Gallery = () => {
+
+  interface ImgProps {
+    url: string;
+  }
+  const Img: React.FC<ImgProps> = ({ url }) => {
+    return (
+      <div className=''>
+        <img
+          src={url}
+          alt=""
+          className='max-w-[15rem] aspect-square rounded-xl shadow-md transform hover:scale-105 duration-300'
+        />
+      </div>
+    );
+  }
   return (
-    <div className='grid justify-center _mx-5 my-24'>
+    <div className='grid gap-6 justify-center m-5 my-24'>
 
       <h1 className='text-3xl'>Explore Our Gallery</h1>
 
-      <div className=' flex justify-items-center'>
+      {/* image card grid */}
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+
+        {/* <Img
+          url="https://plus.unsplash.com/premium_photo-1676299910876-747eeb0c11dc?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3F1YXJlfGVufDB8fDB8fHww"
+        /> */}
+
+        {Array.from({ length: 20 }).map((_, index) => (
+          <Img
+            key={index}
+            url="https://plus.unsplash.com/premium_photo-1676299910876-747eeb0c11dc?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c3F1YXJlfGVufDB8fDB8fHww"
+          />
+        ))}
+
 
       </div>
 
-      <div className='grid grid-cols-2'>
-
-      </div>
-      
     </div>
   )
 }
