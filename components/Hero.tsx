251885f4d90e83de74e34a70d2a98b58f866ca5c
Bruce Wayne <brucethebatmandev@gmail.com>
2024-08-13 13:40:09 +0100

    hero updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,12 +2,12 @@ const Hero = () => {
     return (
         <div className="grid lg:grid-cols-2">
 
-            <div className="col-span-1 bg-blue-900 text-white">
+            <div className="col-span-1 bg-cyan-600 text-white">
 
-                <div className="flex flex-col justify-between items-center py-4 outline-2 outline-pink-400 outline h-full">
+                <div className="flex flex-col gap-4 lg:gap-0 place-items-center h-full outline-2 outline-pink-400 py-6">
 
-                    {/* Navbar */}
-                    <ul className="flex gap-6 text-lg max-w-md w-full outline-2 outline-yellow-500 outline">
+                    {/* Navbar - 20% of the column */}
+                    <ul className="flex gap-6 text-md max-w-md w-full flex-[0_0_5%] _bg-yellow-500 justify-center lg:justify-start _justify-start ">
                         <li>Menu</li>
                         <li>About</li>
                         <li>Gallery</li>
@@ -15,18 +15,18 @@ const Hero = () => {
                         <li>Order</li>
                     </ul>
 
-                    {/* Content */}
-                    <div className="grid gap-10 _justify-items-center max-w-md w-full py-20 outline-2 outline-red-500 outline">
-                        <div className="text-6xl font-semibold">
-                            <h1>We offer a fresh</h1>
+                    {/* Content - 80% of the column */}
+                    <div className="flex flex-col justify-center items-center flex-[0_0_95%] gap-10 lg:gap-14 max-w-md w-full mb-10">
+                        <div className="text-5xl xl:text-6xl font-semibold w-full text-center lg:text-left">
+                            <h1 className="mb-3">We offer a fresh</h1>
                             <h1>Place and Food.</h1>
                         </div>
 
-                        <p className="text-lg">
-                            Pink Kiwi. We offer fresh food and dessert based on high quality ingredients with a perfect place.
+                        <p className="text-lg text-center lg:text-left">
+                            We offer fresh food and dessert based on high quality ingredients with a perfect place.
                         </p>
 
-                        <button className="bg-stone-300 text-gray-800 shadow-lg font-semibold px-5 py-3 rounded-xl max-w-32">
+                        <button className="bg-stone-300 text-gray-800 shadow-lg font-semibold px-5 py-3 rounded-xl w-32">
                             View Menu
                         </button>
                     </div>
@@ -36,10 +36,10 @@ const Hero = () => {
 
             </div>
 
-            <div className="col-span-1 max-w-screen-lg">
+            <div className="col-span-1 _max-w-screen-lg h-full bg-yellow-100">
 
                 {/* Hero Image */}
-                <img src="https://www.riseandgrind.co/wordpress/wp-content/uploads/2021/03/disposable-cup-rise-and-grind-4.jpg" />
+                <img src="https://www.riseandgrind.co/wordpress/wp-content/uploads/2021/03/disposable-cup-rise-and-grind-4.jpg"  className="h-full"/>
             </div>
 
         </div>
