075fcf97f2e2e5c3733039932f3aff86b74b7d09
Bruce Wayne <brucethebatmandev@gmail.com>
2024-08-14 10:22:42 +0100

    updated about section

--- components/About.tsx
+++ components/About.tsx
@@ -4,46 +4,44 @@ import IconWithTextBlock from './IconWithTextBlock'
 
 const About = () => {
   return (
-    <div id='about' className='grid gap-8 justify-items-center mx-5 my-24'>
+    <div id='about' className='my-24 grid lg:grid-cols-2 gap-24 lg:gap-0'>
 
-      <div className='grid gap-4'>
-        <h1 className='text-3xl'>Pink Kiwi Cafe</h1>
-        <p>Fresh Cakes, Vegan Hot Desserts, Italian Gelatos, Sundaes, Premium Milk Shakes, Premium Waffles, Pan Cakes, Fruit Bubble Tea.</p>
-      </div>
+        <div className='col-span-1 m-auto'>
 
-      {/* Container for 3-sections and Ice Cream Image */}
-      <div className='grid gap-16 lg:grid-cols-2 justify-items-center items-center lg:p-10'>
+          <div className='grid gap-10 max-w-md'>
 
-        {/* Section Container - Icon, Header, Text */}
-        <div className='grid gap-8 md:gap-14 max-h-[25rem] max-w-xl'>
+            <div className='grid gap-4'>
+              <h1 className='text-3xl'>What we have to offer</h1>
+              <p>Fresh Cakes, Vegan Hot Desserts, Italian Gelatos, Sundaes, Premium Milk Shakes, Premium Waffles, Pan Cakes, Fruit Bubble Tea.</p>
+            </div>
 
-          <IconWithTextBlock
-            icon={Soup}
-            heading='Organic and Healthy Delights'
-            text='Savor our guilt-free desserts crafted with organic ingredients for a delectable and healthy experience'
-          />
-          <IconWithTextBlock
-            icon={Sandwich}
-            heading='Uncompromising Quality'
-            text='Immerse yourself in desserts that embody exceptional taste and uncompromising quality at every bite'
-          />
-          <IconWithTextBlock
-            icon={Salad}
-            heading='Exquisite Taste Sensations'
-            text='Explore our dessert creations that tantalize the palate with innovative flavors and impeccable craftsmanship'
-          />
+            <IconWithTextBlock
+              icon={Soup}
+              heading='Organic and Healthy Delights'
+              text='Savor our guilt-free desserts crafted with organic ingredients for a delectable and healthy experience'
+            />
+            <IconWithTextBlock
+              icon={Sandwich}
+              heading='Uncompromising Quality'
+              text='Immerse yourself in desserts that embody exceptional taste and uncompromising quality at every bite'
+            />
+            <IconWithTextBlock
+              icon={Salad}
+              heading='Exquisite Taste Sensations'
+              text='Explore our dessert creations that tantalize the palate with innovative flavors and impeccable craftsmanship'
+            />
+          </div>
 
         </div>
 
-        {/* Ice cream Image */}
-        <div className='max-w-[25rem]'>
-          <img 
-            src="https://img.taste.com.au/swuErs2G/taste/2021/02/lemon-dream-dessert-169681-2.jpg" 
-            alt="" 
-            className='rounded-xl shadow-md aspect-square' />
+        <div className='col-span-1 _mx-auto _my-auto m-auto _h-full'>
+          <img
+            src="https://img.taste.com.au/swuErs2G/taste/2021/02/lemon-dream-dessert-169681-2.jpg"
+            // src="https://img.freepik.com/free-photo/gray-grunge-surface-wall-texture-background_1017-18216.jpg"
+            alt=""
+            className='rounded-xl shadow-md aspect-square max-w-[25rem] max-h-[25rem]'
+          />
         </div>
-
-      </div>
 
     </div>
   )
